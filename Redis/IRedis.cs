b3eb94c5f166721aa39632e97f787b44e3650dbc
Saurabhgawade <72007247+Saurabhgawade@users.noreply.github.com>
2024-05-07 18:19:52 +0530

    Add files via upload

---
+++ Redis/IRedis.cs
@@ -0,0 +1,11 @@
+﻿namespace projectDemo.Redis
+{
+    public interface IRedis
+    {
+        Task<T> GetCacheData<T>(string key);
+
+        Task<object> RemoveCacheData(string key);
+
+        Task<bool> SetCacheData<T>(string key, T value, DateTimeOffset exptime);
+    }
+}
---
+++ Redis/RedisService.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using StackExchange.Redis;
+using System.CodeDom;
+
+namespace projectDemo.Redis
+{
+    public class RedisService:IRedis
+    {
+        private readonly IDatabase _database;
+        public RedisService(IConnectionMultiplexer multiplexer)
+        {
+            _database = multiplexer.GetDatabase();
+        }
+        public async Task<T> GetCacheData<T>(string key)
+        {
+            var result =await _database.StringGetAsync(key);
+            if (!string.IsNullOrEmpty(result))
+            {
+                JsonConvert.DeserializeObject<T>(result);
+            }
+            return default;
+        }
+
+        public async Task<object> RemoveCacheData(string key)
+        {
+            var result = await _database.KeyExistsAsync(key);
+            if (result)
+            {
+              return await _database.KeyDeleteAsync(key);
+            }
+            return false;
+        }
+
+        public async Task<bool> SetCacheData<T>(string key, T value, DateTimeOffset exptime)
+        {
+            TimeSpan currtime = exptime.DateTime.Subtract(DateTime.Now);
+
+            return await _database.StringSetAsync(key, JsonConvert.SerializeObject(value), currtime);
+        }
+    }
+}
