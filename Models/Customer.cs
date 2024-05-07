aef22d301ea5ef0b1d3312a7981c338c009de761
Saurabhgawade <72007247+Saurabhgawade@users.noreply.github.com>
2024-05-07 18:25:56 +0530

    Add files via upload

---
+++ Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventory.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public int PostalCode { get; set; }
+
+        
+        public ICollection<Order>? Orders { get; set; }
+    }
+}
---
+++ Models/RegisterUser.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace projectDemo.Models
+{
+    public class RegisterUser
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [DisplayName("Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
